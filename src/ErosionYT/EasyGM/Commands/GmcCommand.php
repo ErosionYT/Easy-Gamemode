e37b88f057e4c87a13e17a6d0f091f8370dc5045
ErosionYT <75473259+ErosionYT@users.noreply.github.com>
2022-01-19 16:35:06 +1100

    API 4.0 Update

--- src/ErosionYT/EasyGM/Commands/GmcCommand.php
+++ src/ErosionYT/EasyGM/Commands/GmcCommand.php
@@ -4,25 +4,33 @@ namespace ErosionYT\EasyGM\Commands;
 
 use ErosionYT\EasyGM\Main;
 
-use pocketmine\Player;
+use pocketmine\player\Player;
+use pocketmine\player\GameMode;
 use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\plugin\Plugin;
 
-class GmcCommand extends PluginCommand{
+class GmcCommand extends Command{
 
-	public function __construct($name, Main $plugin) {
-        parent::__construct($name, $plugin);
-        $this->setDescription("Change your gamemode");
+	public function __construct(string $name) {
+        parent::__construct($name);
+        $this->setPermission("gmc.command");
+        $this->setDescription("Change your gamemode to creative");
+        $this->setAliases(['creative', '1', 'c']);
+        $this->setUsage("/gmc");
     }
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
-		if (!$sender->hasPermission("gmc.command")) {
-			$sender->sendMessage("§cYou do not have permission to use this command");
-			return false;
-		}
-		$sender->setGamemode(Player::CREATIVE);
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("Use the command in-game");
+            return false;
+        }
+        if (!$this->testPermission($sender)) {
+            $sender->sendMessage("§cYou do not have permission to use this command");
+            return false;
+        }
+		$sender->setGamemode(GameMode::CREATIVE());
 		$sender->sendMessage("§6» §7You have changed your gamemode to Creative");
 		return true;
 	}
--- src/ErosionYT/EasyGM/Commands/GmspcCommand.php
+++ src/ErosionYT/EasyGM/Commands/GmspcCommand.php
@@ -4,26 +4,37 @@ namespace ErosionYT\EasyGM\Commands;
 
 use ErosionYT\EasyGM\Main;
 
-use pocketmine\Player;
+use pocketmine\player\Player;
+use pocketmine\player\GameMode;
 use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\plugin\Plugin;
 
-class GmspcCommand extends PluginCommand{
+class GmspcCommand extends Command{
 
-	public function __construct($name, Main $plugin) {
-        parent::__construct($name, $plugin);
-        $this->setDescription("Change your gamemode to spectator");
+    private $plugin;
+
+    public function __construct(string $name) {
+        parent::__construct($name);
+        $this->setPermission("gmspc.command");
+        $this->setDescription("Change your gamemode to Spectator");
+        $this->setAliases(['Spectator', '3', 'spec']);
+        $this->setUsage("/gmspc");
     }
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
-		if (!$sender->hasPermission("gmspc.command")) {
-			$sender->sendMessage("§cYou do not have permission to use this command");
-			return false;
-		}
-		$sender->setGamemode(Player::SPECTATOR);
-		$sender->sendMessage("§6» §7You have changed your gamemode to Spectator");
-		return true;
-	}
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("Use the command in-game");
+            return false;
+        }
+        if (!$this->testPermission($sender)) {
+            $sender->sendMessage("§cYou do not have permission to use this command");
+            return false;
+        }
+
+        $sender->setGamemode(GameMode::SPECTATOR());
+        $sender->sendMessage("§6» §7You have changed your gamemode to Spectator");
+        return true;
+    }
 }
